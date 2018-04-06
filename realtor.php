819ad50ffa110e7299c87c3750f7b5c273483739
John Carroll <john.carroll@ishb.us>
2018-04-06 16:04:46 -0400

    new page

--- realtor.php
+++ realtor.php
@@ -5,26 +5,102 @@ $pathToRoot = "";
 include 'layout/header.php';
 ?>
 <div class='row bottom-pad'>
+    <div class='col-xs-12 text-center'>
+        <h1>Would you like to sell your listings quicker?</h1>
+        <h2>Do you dream of "bidding wars" on properties you are trying to sell?</h2>
+    </div>
+</div>
+<div class='row bottom-pad vertical-align'>
     <div class="col-sm-7 col-xs-12">
         <div class="row bottom-pad">
             <div class="col-xs-12 video-container">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/NvVlXS6ixfk?autoplay=0&rel=0&modestbranding=1" frameborder="0" allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <blockquote><p>If you are interested in the class or have questions <a href="contact-redesign-right-philadelphia-pa.htm">click here</a> to get started or call us at <a href="tel:610-955-8202">610.955.8202</a>.</p></blockquote>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/CycimwK9huU?autoplay=0&rel=0&modestbranding=1" frameborder="0" allowfullscreen></iframe>
             </div>
         </div>
     </div>
     <div class='col-sm-5 col-xs-12'>
-        <h3><strong>Why sign up for our training program?</strong></h3>
-        <ul>
-            <li>The program prepares you using observation and <span class='highlight'>hands-on experience in three client homes</span>; as well as step-by-step classroom training to help launch your new business. </li>
-            <li>This program is <span class='highlight'>live</span>, not online, and <span class='highlight'>limits class size to six students</span>. </li>
-            <li>This program is also ideal for Realtors who wish to provide their clients with home staging solutions within their business model. </li>
-            <li>Debbie has been trained, certified and recognized by three industry trade associations to teach Color, Interior Redesign and Real Estate Home Staging training. She currently runs a successful Redesign and Home Staging business and brings plenty of business knowledge, life experiences, and fun energy to the training classroom.</li>
-            <li>This course is endorsed by the Association of Design Education (ADE).</li>
-        </ul>
+        <p>
+            Let Redesign Right help you create first impressions that count! As almost 95% of home viewing is done on the internet, it is imperative that the home visually stand out. With such a competitive market, we specialize in staging homes that buyers WILL REMEMBER!
+        </p>
+        <p>
+            We have been in the interior decorating, and home staging business for over 15 years and enjoy a proven track record of successes with many realtors from the Chester and Delaware counties.
+        </p>
+    </div>
+</div>
+<div class='row bottom-pad'>
+    <div class='col-xs-12'>
+        <h1>We offer two Staging Options:</h1>
+    </div>
+</div>
+<div class='row bottom-pad'>
+    <div class='col-xs-12'>
+        <p>
+            <strong>1. DIY Option</strong>
+        </p>
+         <p>
+             We will meet with your homeowner and generate an honest and thorough 'to do' list. (Includes repairs, removal and redesign for the homeowner to complete on their own.) This option is for the homeowner who prefers to do it themselves after Redesign Right provides the direction.
+        </p>
+         <p>
+             This option is <strong>the MOST popular Realtor choice,</strong> and we offer you a special flat rate fee of only $200-300 per consult.
+        </p>
+    </div>
+</div>
+<div class='row bottom-pad'>
+    <div class='col-xs-12'>
+        <p>
+            <strong>2. Physical One-Day Staging option:</strong>
+        </p>
+         <p>
+             This staging option is an affordable one-day total transformation package, where the homeowner leaves the house and a team of Redesign Right certified stagers would physically create a 'model home' in one day using what is already there. This option includes redesigning furniture, art, and accessories. After the redesigners complete the one-day staging, the home is ready for professional photographing and sale.
+        </p>
+         <p>
+             This option appeals to the homeowner who does not have the time or is unable to stage their home on their own. Pricing of this service is by room and determined at the date of consultation with the homeowner. Sometimes additional items must be purchased to complete the staging process effectively.
+        </p>
+    </div>
+</div>
+<div class='row bottom-pad'>
+    <div class='col-xs-12 text-center'>
+        <h1>Become a Realtor they remember!</h1>
+        <h2>Are you tired of giving the traditional Keurig coffee maker as a closing gift?</h2>
+        <h2>Many of our realtors give their clients the 'Gift of Design.'</h2>
+    </div>
+</div>
+<div class='row bottom-pad'>
+    <div class='col-xs-12'>
+        <img class='img-responsive image-center' src='images/gift-cert.png'>
+    </div>
+</div>
+<div class='row bottom-pad'>
+    <div class='col-xs-12'>
+        <p>
+            Since homeowners already know Redesign Right from the staging consult, they are thrilled to have 1-3 hours of design services to help them get settled into their <strong>NEW</strong> home! You helped them get the house, now assist them to turn it into a home!
+        </p>
+        <p>
+            <a href='about-debbie-correale.htm'>Debbie Correale</a> is a ten-time award-winning interior redesigner, and professional home stager and trainer.
+        </p>
+    </div>
+</div>
+<div class="row bottom-pad">
+    <div class="col-xs-12">
+        <blockquote><p>If you are interested in the class or have questions <a href='contact-redesign-right-philadelphia-pa.htm'>click here</a> to get started or call us at <a href='tel:610-955-8202'>610.955.8202</a>.</p></blockquote>
+    </div>
+</div>
+<div class='row bottom-pad'>
+    <div class='col-xs-12'>
+        <h1>What other Realtors are saying about Redesign Right’s Staging Services:</h1>
+        <p>
+            <strong>Liz from Re/Max Preferred</strong> states: <i>“I have always been impressed with Debbie&#39;s professionalism, style and her &quot;way&quot; of relating to my clients. I have met and in the past worked with other stager-designers, and I can honestly say, there is no one better out there than Redesign Right!”</i>
+        </p>
+        <p>
+            <strong>Lynn from Berkshire Hathaway, Prudential, Fox &AMP; Roach</strong> states: <i>&quot;Her keen eye for detail and staging a home to sell has made all the difference in my listings! Redesign Right is professional and effective in getting properties sold faster!”</i>
+        </p>
+        <p>
+            <strong>Jeff Bollinger from Berkshire Hathaway Home Services, Fox &amp; Roach Realtors</strong> states: <i>&quot;When I find an outstanding Home Stager, I stick with her! Debbie Correale is that person. The goal is to create not only a great First Impression but also a lasting impression that will make the Buyer feel that they have found their ideal home. I call it the &quot;&#39;emotional pull&quot; that comes when everything just feels Right. From furniture arrangement to the right colors, to the Right accessories, Debbie does it to perfection.&quot;</i>
+        </p>
+    </div>
+</div>
+<div class="row bottom-pad">
+    <div class="col-lg-12 col-xs-12 disclaimer">
+        <p>We are currently serving the Philadelphia, Pennsylvania area including Chester, Delaware, Montgomery, Philadelphia, Lancaster and Lehigh counties, also select areas of Delaware and New Jersey.</p>
     </div>
 </div>
